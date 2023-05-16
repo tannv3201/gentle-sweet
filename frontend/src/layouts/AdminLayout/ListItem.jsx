570aad07f50d31774e3b59627cfc840ae5b237ee
Tan Nguyen Van <108793627+tannv3201@users.noreply.github.com>
2023-05-16 13:43:03 +0700

    Update ListItem.jsx

--- frontend/src/layouts/AdminLayout/ListItem.jsx
+++ frontend/src/layouts/AdminLayout/ListItem.jsx
@@ -3,69 +3,72 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PeopleIcon from "@mui/icons-material/People";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import LayersIcon from "@mui/icons-material/Layers";
-import AssignmentIcon from "@mui/icons-material/Assignment";
+
 import styles from "./DefaultLayout.module.scss";
 import classNames from "classnames/bind";
 import MenuItem from "./MenuItem";
-
+import {
+    CategoryRounded,
+    ShoppingCart,
+    People,
+    BarChart,
+    Layers,
+    Assignment,
+    Dashboard,
+} from "@mui/icons-material";
 export const mainListItems = (
     <React.Fragment>
         <MenuItem
             to={"/admin"}
             title={"Dashboard"}
-            icon={<DashboardIcon />}
+            icon={<Dashboard />}
             end={true}
         />
         <MenuItem
             to={"/admin/admin-user"}
             title={"Nhân viên"}
-            icon={<PeopleIcon />}
+            icon={<People />}
         />
         <MenuItem
             to={"/admin/customer"}
             title={"Khách hàng"}
-            icon={<PeopleIcon />}
+            icon={<People />}
         />
         <MenuItem
             to={"/admin/product-category"}
             title={"Danh mục sản phẩm"}
-            icon={<DashboardIcon />}
+            icon={<CategoryRounded />}
         />
         <MenuItem
             to={"/admin/service-category"}
             title={"Danh mục dịch vụ"}
-            icon={<DashboardIcon />}
+            icon={<CategoryRounded />}
         />
         <MenuItem
             to={"/admin/product"}
             title={"Sản phẩm"}
-            icon={<DashboardIcon />}
+            icon={<Dashboard />}
         />
         {/* <MenuItem
             to={"/admin/productImage"}
             title={"Hình ảnh"}
-            icon={<DashboardIcon />}
+            icon={<Dashboard />}
         /> */}
         <ListItemButton>
             <ListItemIcon>
-                <ShoppingCartIcon />
+                <ShoppingCart />
             </ListItemIcon>
             <ListItemText primary="Đơn hàng" />
         </ListItemButton>
         <ListItemButton>
             <ListItemIcon>
-                <BarChartIcon />
+                <BarChart />
             </ListItemIcon>
             <ListItemText primary="Báo cáo" />
         </ListItemButton>
         <ListItemButton>
             <ListItemIcon>
-                <LayersIcon />
+                <Layers />
             </ListItemIcon>
             <ListItemText primary="Integrations" />
         </ListItemButton>
@@ -79,19 +82,19 @@ export const secondaryListItems = (
         </ListSubheader>
         <ListItemButton>
             <ListItemIcon>
-                <AssignmentIcon />
+                <Assignment />
             </ListItemIcon>
             <ListItemText primary="Current month" />
         </ListItemButton>
         <ListItemButton>
             <ListItemIcon>
-                <AssignmentIcon />
+                <Assignment />
             </ListItemIcon>
             <ListItemText primary="Last quarter" />
         </ListItemButton>
         <ListItemButton>
             <ListItemIcon>
-                <AssignmentIcon />
+                <Assignment />
             </ListItemIcon>
             <ListItemText primary="Year-end sale" />
         </ListItemButton>
