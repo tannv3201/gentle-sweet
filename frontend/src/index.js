4f4dca4cd8ba2105658a2229272d0e10a381e7c4
Tan Nguyen Van <108793627+tannv3201@users.noreply.github.com>
2023-06-13 22:23:31 +0700

    update api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,10 @@ import "./assets/scss/Global.scss";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// axios.defaults.baseURL = "http://localhost:8080/v1";
+axios.defaults.baseURL = "http://localhost:8080/";
 root.render(
     // <React.StrictMode>
     <Provider store={store}>
