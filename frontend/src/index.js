f98c1986efe241f85408f20b9078ebddb569d833
Tan Nguyen Van <108793627+tannv3201@users.noreply.github.com>
2023-06-18 03:16:19 +0700

    UPDATE API BASE URL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,8 @@ import { persistor, store } from "./redux/store";
 import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// axios.defaults.baseURL = "http://localhost:8080/";
-axios.defaults.baseURL = "http://192.168.1.1:8080/";
+axios.defaults.baseURL = "http://localhost:8080/";
+// axios.defaults.baseURL = "http://192.168.1.1:8080/";
 
 root.render(
     // <React.StrictMode>
