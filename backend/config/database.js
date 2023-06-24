3a5ffea37a1c5b10d2233c1f7a09cbee686ea3b7
Tan Nguyen Van <108793627+tannv3201@users.noreply.github.com>
2023-06-24 17:30:38 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,31 @@
+// const mysql = require("mysql2/promise");
+
+// const pool = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "123456",
+//     // database: "gentle_v1",
+//     database: "gentle_v1",
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+// });
+
+// module.exports = pool;
+
+// Deployment
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "123456",
+    host: "gentle-database-v1-do-user-14273017-0.b.db.ondigitalocean.com",
+    user: "doadmin",
+    password: "AVNS_4gvS8U6Kd8vfRVpPhUW",
     // database: "gentle_v1",
     database: "gentle_v1",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    port: 25060,
 });
 
 module.exports = pool;
