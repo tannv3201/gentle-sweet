40349b5b725090baf616944c7ca22986ae439761
Tan Nguyen Van <108793627+tannv3201@users.noreply.github.com>
2023-07-05 12:45:56 +0700

    fix query getBookingTimeList

--- backend/controllers/bookingDetailController.js
+++ backend/controllers/bookingDetailController.js
@@ -58,6 +58,7 @@ const bookingController = {
                 );
                 return !matchingBooking;
             });
+
             return res.status(200).json(filteredOptions);
         } catch (error) {
             res.status(500).json(error);
